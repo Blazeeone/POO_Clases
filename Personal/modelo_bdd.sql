548679901adeed34647b644dc7b026eba890997b
Yoshua Castillo <yoshua.castillo@inacapmail.cl>
2025-11-13 11:17:20 -0300

    creacion de mascota

---
+++ Personal/modelo_bdd.sql
@@ -0,0 +1,30 @@
+/*
+
+-- Nombre
+-- Atributos: TIPO DE DATOS (SQL) (CONSTRAINT)
+
+MASCOTA 
+id_macota interger PRIMARY KEY,
+nombre varchar(50),
+edad interger,
+especie varchar(30)
+
+PERRO
+nombre varchar(50),
+raza varchar(30),
+edad interger,
+color varchar(20)
+
+
+GATO
+nombre varchar(50),
+raza varchar(30),
+color varchar(20)
+
+AVE
+nombre varchar(50),
+especie varchar(30),
+color varchar(20)
+
+*/
+ 
--- oracle_ejemplo/ejemplo_crud.py
+++ oracle_ejemplo/ejemplo_crud.py
@@ -10,21 +10,24 @@ password = os.getenv("ORACLE_PASSWORD")
 def get_connection():
     return oracledb.connect(iuser=username, password=password, dsn=dsn)
 
-def create_table_personas():
-    query = (
-        "CREATE TABLE personas ("
-        "rut  VARCHAR2(50) PRIMARY KEY,"
-        "nombres"
-        "apellidos varchar2(200) personas ("
-        "fehca_nacimiento DATE,"
-        "cod_area VARCHAR2(20),"
-        "numero_telefono VARCHAR2(50)"
-    )
 
-    try: 
-        with get_connection() as conn: 
-            with conn.cursor() as cur: 
-                cur.execute(query) 
-                print("Tabla 'personas' creada.") 
-    except oracledb.DatabaseError as error: 
-        print(f"No se pudo crear la tabla: {error}") 
+def create_schrema():
+    
+    tables = [
+        (
+            "CREAATE TABLE"
+            "MASCOTAS ("
+            "id INTEGER PRIMARTY KEY,"
+            "nombre VARCHAR(50),"
+            "edad INTEGER,"
+            "especie VARCHAR(30)"
+
+            "CREATE TABLE"
+            "DUEÑOS ("
+            "id INTEGER PRIMARY KEY,"
+            "nombre VARCHAR(50),"
+            "telefono VARCHAR(15),"
+            "direccion VARCHAR(100)"
+
+        )
+    ]
