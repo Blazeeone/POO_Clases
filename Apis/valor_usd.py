f97b42f04d907cc58a5b2b0e2dd04f55bacec44f
Yoshua Castillo <yoshua.castillo@inacapmail.cl>
2025-12-05 11:13:48 -0300

    Create valor_usd.py

---
+++ Apis/valor_usd.py
@@ -0,0 +1,16 @@
+import requests
+# [Donde esta la API]-----
+base_url = "https://cl.dolarapi.com"
+
+# Ruta o Endpoint 
+endpoint_dolar = "/v1/cotizaciones/usd"
+
+# Juntamos la url y su endpoint para realizar una peticion de tipo GET
+respuesta = requests.get(f"{base_url}{endpoint_dolar}")
+# ⬆️ https://cl.dolarapi.com/v1/cotizaciones/usd
+
+# Serializamos la informacion en JSON para trabajar de forma estructurada
+data = respuesta.json()
+
+# Mostramos la data rescatada de la respuesta
+print(data)
